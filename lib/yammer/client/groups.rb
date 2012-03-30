cf3960c03c119e8adb09da05236c88ee86509331
y_benjo <ybenjo.repose@gmail.com>
2012-03-30 23:38:03 +0900

    fix parameters.

--- lib/yammer/client/groups.rb
+++ lib/yammer/client/groups.rb
@@ -12,8 +12,8 @@ module Yammer
       # @option options [Integer] :page 20 groups will be shown per page.
       # @option options [String] :letter Return groups beginning with the given letter.
       # @option options [messages|members|privacy|created_at|creator] :sort_by Results will be returned sorted by one of the above options instead of the default behavior of sorting by number of messages.
-      # @option options [Boolean] :reverse Return results in reverse order if :reverse => true.
-      # @option options [Boolean] :private Used when creating or updating groups to make them private.
+      # @option options [String] :reverse Return results in reverse order if uses true.
+      # @option options [String] :private Used when creating or updating groups to make them private if uses true.
       # @return [Hashie::Mash] Informations of groups.
       def get_groups(options={})
         get("groups", options)
@@ -26,7 +26,7 @@ module Yammer
       # @rate_limited unconfirmed
       # @param id [Integer] The id of specific group.
       # @return [Hashie::Mash] Informations of group which id is #{id}.
-      def get_group(id, options = { })
+      def get_group(id, options={})
         get("groups/#{id}", options)
       end
 
@@ -35,9 +35,9 @@ module Yammer
       # @authenticated true
       # @rate_limited unconfirmed
       # @param name [String] The name of group.
-      # @param private [Boolean] Used true when creating or updating groups to make them private.
-      def create_group(name, private = false)
-        post("groups", {:name => name, :private => private})
+      # @param private [String] Used true when creating or updating groups to make them private.
+      def create_group(name, options={})
+        post("groups", options.merge(:name => name))
       end
     end
   end
